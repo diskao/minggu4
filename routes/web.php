e538a79879d5fe56b6105f749ee7d77223f7a11a
Diskaoktavaia <diskaoktavia36@gmail.com>
2021-09-25 15:05:01 +0700

    membuat model dan menampilkan isi data ke view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class, 'getAll']);
+Route::get('/article/{id}', [HomeController::class, 'getById']);
